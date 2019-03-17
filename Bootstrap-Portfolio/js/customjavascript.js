76d9d1c0686f94232509388c9e847112e7a01fe6
Obenny <34409710+Obenny@users.noreply.github.com>
2019-03-18 06:59:04 +0800

    Bootstrap-Portfolio
    Bootstrap-Portfolio

---
+++ Bootstrap-Portfolio/js/customjavascript.js
@@ -0,0 +1,118 @@
+$(function()
+{
+
+  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      event.preventDefault(); // prevent default submit behaviour
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var phone = $("input#phone").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $this = $("#sendMessageButton");
+      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+      $.ajax({
+        url: "././mail/contact_me.php",
+        type: "POST",
+        data: {
+          name: name,
+          phone: phone,
+          email: email,
+          message: message
+        },
+        cache: false,
+        success: function() {
+          // Success message
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-success')
+            .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+            .append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        complete: function() {
+          setTimeout(function() {
+            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+          }, 1000);
+        }
+      });
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+  $('#success').html('');
+});
+
+//automatic dropdown menubar
+$(function() 
+{
+    $('.dropdown').hover(
+        function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+        },
+        function() {
+            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+        }
+    );
+
+    $('.dropdown-menu').hover(
+        function() {
+            $(this).stop(true, true);
+        },
+        function() {
+            $(this).stop(true, true).delay(200).fadeOut();
+        }
+    );
+});
+
+//right-click
+  function disableClick()
+  {
+	var message = "Not Allowed";
+	function rtclickcheck(keyp)
+	{ 
+		if (navigator.appName == "Netscape" && keyp.which == 3)
+		{ 
+		alert(message); return false; 
+		}
+
+		if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) 
+		{ 
+			alert(message); 
+			return false; 
+		} 
+	}
+	document.onmousedown = rtclickcheck;
+  }
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Bootstrap-Portfolio
+
+This is a basic responsive portfolio build on Bootstrap for freshers who intend showcasing their work on a single page.
+
+You can fine a live example on [T. A. M] - (http://www.bluedrupsltd.com/isaacobenson/)
+
+Each project contains an external photo and a title which links to the details of the
+project done when clicked. The details cantains different sub-headings, images
+and buttons indicating whether to demo project, download project or close project details. 
+
+## Getting Started
+
+These instructions will get you running this template on your local machine for development/testing purposes. 
+See Prerequisites and Installation for notes on how to deploy the project on a live system.
+
+### Prerequisites and Installation
+
+Note: This templete was tested on XAMPP.
+
+To begin using this template:
+[Download XAMPP and install](https://www.apachefriends.org/download.html) or you can open the `index.html` file in your web browser.
+
+Edit the `index.html`, the CSS and customjavascript files included with the template in your text editor to make changes. 
+
+## Running the tests
+
+Just start your Apache and MySQL from your XAMPP Control Panel and then type localhost/directory of SimplePortfolio in your XAMPP's htdocs/index.html on your browser.
+
+## Deployment
+
+To deploy this on a live system, make appropriate changes to the index.html, the CSS and customjavascript files 
+so that it can look awesome like that of [T. A. M] - (http://www.bluedrupsltd.com/isaacobenson/)
+
+## Built With Template from
+
+* [Startbootstrap](http://startbootstrap.com/template-overviews/freelancer/) 
+
+
+## Author(s)
+
+* **Isaac Obenson** - *Initial work* - [Obenny](https://github.com/Obenny)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to [Startbootstrap](http://startbootstrap.com/) and anyone whose idea was used.
